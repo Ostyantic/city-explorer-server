559004214e9a9f7c31c94bfb95cf2ba6e56ec948
Anthony Lopez <Antl21292@gmail.com>
2023-01-28 23:28:41 -0500

    Feature 2 back-end API Task List complete

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 // express server library
 const express = require('express');
 
-//initializing express library
+//initializes express library
 const app = express();
 
 // cors library
@@ -17,12 +17,64 @@ const cors = require('cors');
 // controls which websites can request access to server
 app.use(cors());
 
-//initializing the port will run on
+//initializes the port will run on
 const PORT = process.env.PORT || 3002;
+
+//alloww access to weather.json
+const weatherData = require('./data/weather.json');
 
 app.get('/', (req, res) => {
   res.send('Endpoint is working');
 });
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.get('/weatherData', (req,res) => {
+  res.send(weatherData);
+});
 
+app.get('/weather', (req,res) => {
+
+  // console logging the query parameter and defining searchQuery with the value of req(uest).query.searchQuery
+  console.log(req.query);
+  let searchQuery = req.query.searchQuery;
+
+  console.log('City:', searchQuery);
+
+  //creating a new Forecast class
+  let locationForecast = new Forecast(searchQuery);
+
+  // defining a varable and assigning it the value returned from the newly create Forecast class' method.
+  let locationDateAndDesc = locationForecast.getDateAndDesc();
+
+  // console logging the date and desc variable and sending the API response to the client
+  console.log(locationDateAndDesc);
+  res.status(200).send(locationDateAndDesc);
+
+});
+
+//creating a forecast class
+class Forecast{
+
+  // using a constructor to create the class and passing searchQuery as the parameter
+  constructor(searchQuery){
+
+    // defining a "weather" variable and assigning it the value of the property found within the array.find() method
+    // defining a property in the constructor and assigning it the value of the data array within the searched weather object
+
+    let weather = weatherData.find(obj => obj.city_name === searchQuery);
+    this.weatherForecast = weather.data;
+
+    // individually assigning date and desc properties
+    // this.date = weather.data.map(element => element.datetime);
+    // this.desc = weather.data.map(element => element.weather.description);
+  }
+
+  // function that maps over weather forecast and returns date and description properties per element in the weatherForecast array
+  getDateAndDesc(){
+    return this.weatherForecast.map(element =>{
+      return {date: element.datetime, desc: element.weather.description};
+    });
+  }
+
+}
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
