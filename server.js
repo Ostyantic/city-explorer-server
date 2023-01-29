c12ae566fc2e462b7e0445bcf3b51e63a252e722
Anthony Lopez <Antl21292@gmail.com>
2023-01-28 19:48:49 -0500

    default setup

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// allows the use of the variables within .env file
+// .env access
+require('dotenv').config();
+
+// express server library
+const express = require('express');
+
+//initializing express library
+const app = express();
+
+// cors library
+const cors = require('cors');
+
+// express app that uses cors
+// controls which websites can request access to server
+app.use(cors());
+
+//initializing the port will run on
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (req, res) => {
+  res.send('Endpoint is working');
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
