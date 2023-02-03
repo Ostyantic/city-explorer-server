03dea1032bc0e385186b56048c1b2d145b4891fe
Anthony Lopez <Antl21292@gmail.com>
2023-02-03 01:31:42 -0500

    Movie DB API hooked up to server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 // express server library
 const express = require('express');
+const axios = require('axios');
 
 //initializes express library
 const app = express();
@@ -22,7 +23,7 @@ const PORT = process.env.PORT || 3002;
 
 //alloww access to weather.json
 const weatherData = require('./data/weather.json');
-const { default: axios } = require('axios');
+
 
 app.get('/', (req, res) => {
   res.send('Endpoint is working');
@@ -36,38 +37,44 @@ app.get('/weather', getWeather);
 
 async function getWeather(req,res,next){
 
-
-  // console logging the query parameter and defining searchQuery with the value of req(uest).query.searchQuery
+  // console logging the query parameter and defining city with the value of req(uest).query.city
   console.log(req.query);
   let city = req.query.city;
+  let cityLat = req.query.lat;
+  let cityLon = req.query.lon;
 
-  let url = `https://api.weatherbit.io/v2.0/current?key=${process.env.WEATHER_API_KEY}&city=${city}`;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${cityLat}&lon=${cityLon}&days=7`;
 
   try{
     const weatherResponse = await axios.get(url);
     console.log(weatherResponse.data);
-    res.status(200).send(weatherResponse.data);
+    let formattedWeather = weatherResponse.data.data.map(city => new Forecast(city));
+    res.status(200).send(formattedWeather);
+  }
+  catch(error){
+    res.status(500).send('Server Error!');
+  }
+}
+
+app.get('/movie', getMovie);
+
+async function getMovie(req,res,next){
+
+  console.log(`this it the request: ${req}`);
+  let city = req.query.city;
+
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
+  console.log(`This is the url: ${url}`);
+
+  try{
+    const movieResponse = await axios.get(url);
+    console.log(movieResponse.data);
+    const formattedMovieResponse = movieResponse.data.results.map(obj => new Movie(obj));
+    res.status(200).send(formattedMovieResponse);
   }
   catch(error){
     res.status(500).send('Server Error');
   }
-  // console.log('City:', searchQuery);
-  // let lat = req.query.lat;
-  // let lon = req.query.lon;
-
-  // console logging lat and lon
-  // console.log('Lat: ', lat);
-  // console.log('Lon: ', lon);
-
-  //creating a new Forecast class
-  let locationForecast = new Forecast(searchQuery);
-
-  // defining a varable and assigning it the value returned from the newly create Forecast class' method.
-  let locationDateAndDesc = locationForecast.getDateAndDesc();
-
-  // console logging the date and desc variable and sending the API response to the client
-  console.log(locationDateAndDesc);
-  res.status(200).send(locationDateAndDesc);
 }
 
 
@@ -76,28 +83,24 @@ async function getWeather(req,res,next){
 //creating a forecast class
 class Forecast{
 
-  // using a constructor to create the class and passing searchQuery as the parameter
-  constructor(searchQuery){
-
-    // defining a "weather" variable and assigning it the value of the property found within the array.find() method
-    // defining a property in the constructor and assigning it the value of the data array within the searched weather object
-
-    let weather = weatherData.find(obj => obj.city_name === searchQuery);
-    this.weatherForecast = weather.data;
-    // this.lat = weather.lat;
-    // this.lon = weather.lon;
-    // individually assigning date and desc properties
-    // this.date = weather.data.map(element => element.datetime);
-    // this.desc = weather.data.map(element => element.weather.description);
+  constructor(city){
+    this.date = city.datetime;
+    this.desc = city.weather.description;
   }
+}
 
-  // function that maps over weather forecast and returns date and description properties per element in the weatherForecast array
-  getDateAndDesc(){
-    return this.weatherForecast.map(element =>{
-      return {date: element.datetime, desc: element.weather.description};
-    });
+// movie class
+
+class Movie{
+  constructor(movie){
+    this.title = movie.title;
+    this.overview = movie.overview;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.img_url = movie.poster_path;
+    this.popularity = movie.popularity;
+    this.release_date = movie.release_date;
   }
-
 }
 
 
